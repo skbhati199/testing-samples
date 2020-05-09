81945012da15320ad19ea8f26532826f0c0a26a3
Sonu Kumar <skbhati199@gmail.com>
2020-05-09 17:10:25 +0530

    Update RecyclerViewSampleTest.java

--- ui/espresso/RecyclerViewSample/app/src/androidTest/java/com/example/android/testing/espresso/RecyclerViewSample/RecyclerViewSampleTest.java
+++ ui/espresso/RecyclerViewSample/app/src/androidTest/java/com/example/android/testing/espresso/RecyclerViewSample/RecyclerViewSampleTest.java
@@ -48,21 +48,15 @@ public class RecyclerViewSampleTest {
 
     private static final int ITEM_BELOW_THE_FOLD = 40;
 
-    /**
-     * Use {@link ActivityScenario} to create and launch the activity under test. This is a
-     * replacement for {@link androidx.test.rule.ActivityTestRule}.
-     */
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule =
         new ActivityScenarioRule<MainActivity>(MainActivity.class);
 
     @Test
     public void scrollToItemBelowFold_checkItsText() {
-        // First scroll to the position that needs to be matched and click on it.
         onView(ViewMatchers.withId(R.id.recyclerView))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(ITEM_BELOW_THE_FOLD, click()));
 
-        // Match the text in an item below the fold and check that it's displayed.
         String itemElementText = getApplicationContext().getResources().getString(
                 R.string.item_element_text) + String.valueOf(ITEM_BELOW_THE_FOLD);
         onView(withText(itemElementText)).check(matches(isDisplayed()));
@@ -70,19 +64,15 @@ public class RecyclerViewSampleTest {
 
     @Test
     public void itemInMiddleOfList_hasSpecialText() {
-        // First, scroll to the view holder using the isInTheMiddle matcher.
         onView(ViewMatchers.withId(R.id.recyclerView))
                 .perform(RecyclerViewActions.scrollToHolder(isInTheMiddle()));
 
-        // Check that the item has the special text.
         String middleElementText =
                 getApplicationContext().getResources().getString(R.string.middle);
         onView(withText(middleElementText)).check(matches(isDisplayed()));
     }
 
-    /**
-     * Matches the {@link CustomAdapter.ViewHolder}s in the middle of the list.
-     */
+
     private static Matcher<CustomAdapter.ViewHolder> isInTheMiddle() {
         return new TypeSafeMatcher<CustomAdapter.ViewHolder>() {
             @Override
